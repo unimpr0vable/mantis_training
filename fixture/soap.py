8e90456816dbd4aa12f0dddf7c7e58a42dfa1330
gurovaae <albina.gurova@akbarsdigital.ru>
2022-06-20 17:57:04 +0300

    Fix address_soap

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app = app
 
     def get_project_list(self):
-        client = Client('http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl')
+        client = Client(self.app.base_url + 'api/soap/mantisconnect.php?wsdl')
         project_list = []
         resp = client.service.mc_projects_get_user_accessible(username=self.app.config["username"],
                                                               password=self.app.config["password"])
